c4732869d5d063e6d60b4c55a9d007c390f5c15a
dakitten2357 <jay.w.murray@gmail.com>
2018-02-09 12:50:18 -0500

    initial commit for SwipePlayerController reparented FroggerPlayerController to SwipePlayerController

---
+++ Source/Frogger/Public/SwipePlayerController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "SwipePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FROGGER_API ASwipePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
