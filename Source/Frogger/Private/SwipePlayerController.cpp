8a75eadf9f64549af8067410704798dd8b65ba09
dakitten2358 <jay.w.murray@gmail.com>
2018-02-10 08:26:18 -0500

    basic swipe controller

--- Source/Frogger/Private/SwipePlayerController.cpp
+++ Source/Frogger/Private/SwipePlayerController.cpp
@@ -1,7 +1,94 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SwipePlayerController.h"
+#include "GameFramework/Pawn.h"
+#include "NumericLimits.h"
 
+std::map<ESwipeDirection, FVector> ASwipePlayerController::DirectionMap = 
+{
+	{ ESwipeDirection::SD_Up, FVector(0, -1, 0) },
+	{ ESwipeDirection::SD_Right, FVector(1, 0, 0) },
+	{ ESwipeDirection::SD_Down, FVector(0, 1, 0) },
+	{ ESwipeDirection::SD_Left, FVector(-1, 0, 0) }
+};
 
+void ASwipePlayerController::SetupInputComponent()
+{
+	APlayerController::SetupInputComponent();
 
+	check(InputComponent);
+	InputComponent->BindTouch(EInputEvent::IE_Pressed, this, &ASwipePlayerController::SwipeBegan);
+	InputComponent->BindTouch(EInputEvent::IE_Released, this, &ASwipePlayerController::SwipeEnded);
+}
 
+void ASwipePlayerController::SwipeBegan(ETouchIndex::Type fingerIndex, FVector location)
+{
+	RecordedTouchBegins.FindOrAdd(fingerIndex) = location;
+}
+
+void ASwipePlayerController::SwipeEnded(ETouchIndex::Type fingerIndex, FVector location)
+{
+	// were we recording a touch for this finger?
+	auto pInitialLocation = RecordedTouchBegins.Find(fingerIndex);
+	if (pInitialLocation)
+	{
+		auto initialLocation = *pInitialLocation;
+		
+		// get the direction vector
+		auto direction = (location - initialLocation);
+		direction.Normalize();
+
+		// find the nearest cardinal direction
+		auto cardinalDirection = FindNearestCardinalDirection(direction);
+
+		// send out the raw input, just in case someone wants it
+		SwipeInputRaw(initialLocation, location, cardinalDirection);
+				
+		// make sure we clear our the key we're using to track this touch
+		RecordedTouchBegins.Remove(fingerIndex);
+	}
+}
+
+ESwipeDirection ASwipePlayerController::FindNearestCardinalDirection(const FVector& directionVector) const
+{
+	auto nearestDirection = ESwipeDirection::SD_None;
+	auto closestDistanceSquared = MAX_flt;
+	for (const auto& p : DirectionMap)
+	{
+		auto cardinalDirection = p.first;
+		const auto& cardinalVector = p.second;
+
+		auto distanceSquared = FVector::DistSquared(cardinalVector, directionVector);
+		if (distanceSquared < closestDistanceSquared)
+		{
+			nearestDirection = cardinalDirection;
+			closestDistanceSquared = distanceSquared;
+		}
+	}
+
+	return nearestDirection;
+}
+
+bool ASwipePlayerController::GetWorldspaceLocation(const FVector& screenLocation, FVector& worldLocation) const
+{
+	worldLocation = FVector::ZeroVector;
+	FVector worldDirection = FVector::ZeroVector;
+
+	bool success = DeprojectScreenPositionToWorld(screenLocation.X, screenLocation.Y, worldLocation, worldDirection);
+	if (!success)
+	{
+		// figure out where this intersects with the plane that the player is on
+		const FVector& lineStart = worldLocation;
+		FVector lineEnd = worldLocation + (worldDirection * 1000000);
+		FVector planeOrigin = FVector::ZeroVector;
+		if (auto pawn = GetPawn())
+		{
+			planeOrigin = pawn->GetActorLocation();
+			auto locationOnPlane = FMath::LinePlaneIntersection(lineStart, lineEnd, planeOrigin, FVector::UpVector);
+			worldLocation = locationOnPlane;
+			return true;
+		}
+	}
+
+	return false;
+}
--- Source/Frogger/Public/SwipePlayerController.h
+++ Source/Frogger/Public/SwipePlayerController.h
@@ -4,11 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include <map>
 #include "SwipePlayerController.generated.h"
 
 UENUM(BlueprintType)
 enum class ESwipeDirection : uint8
 {
+	SD_None,
 	SD_Up,
 	SD_Right,
 	SD_Down,
@@ -22,12 +24,28 @@ UCLASS()
 class FROGGER_API ASwipePlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+protected:
+	virtual void SetupInputComponent() override;
 	
 public:
 	UFUNCTION(BlueprintImplementableEvent, meta=(DisplayName="Swipe Input (RAW)"))
-	void SwipeInputRaw(FVector2D Origin, FVector2D Destination);
+	void SwipeInputRaw(FVector Origin, FVector Destination, ESwipeDirection Direction);
 
+	/*
 	UFUNCTION(BlueprintImplementableEvent, meta = (DisplayName = "Swipe Input"))
-	void SwipeInput(FVector2D InputVector, ESwipeDirection Direction);
+	void SwipeInput(FVector InputVector, ESwipeDirection Direction);
+	*/
+
+private:
+	void SwipeBegan(ETouchIndex::Type fingerIndex, FVector location);
+	void SwipeEnded(ETouchIndex::Type fingerIndex, FVector location);
+
+	TMap<ETouchIndex::Type, FVector> RecordedTouchBegins;
+
+	bool GetWorldspaceLocation(const FVector& screenLocation, FVector& outWorldLocation) const;
+	ESwipeDirection FindNearestCardinalDirection(const FVector& directionVector) const;
+
+private:
+	static std::map<ESwipeDirection, FVector> DirectionMap;
 };
