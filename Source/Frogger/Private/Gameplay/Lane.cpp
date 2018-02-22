a5a64230ef469c55ad0d0fb889e790c216b8676a
dakitten2358 <jay.w.murray@gmail.com>
2018-02-21 21:43:32 -0500

    better component setup for lane

--- Source/Frogger/Private/Gameplay/Lane.cpp
+++ Source/Frogger/Private/Gameplay/Lane.cpp
@@ -4,6 +4,8 @@
 #include "Runtime/Engine/Classes/Components/BoxComponent.h"
 #include "Runtime/Engine/Public/TimerManager.h"
 #include "Kismet/GameplayStatics.h"
+#include "Components/ArrowComponent.h"
+#include "DisplayDebugHelpers.h"
 
 // Sets default values
 ALane::ALane(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
@@ -11,10 +13,34 @@ ALane::ALane(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitiali
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	DespawnArea = ObjectInitializer.CreateDefaultSubobject<UBoxComponent>(this, TEXT("StartArea"));
-	check(DespawnArea);
+	// Event is fired when the player enters the Lane (scoring, etc)
+	LaneArea = ObjectInitializer.CreateDefaultSubobject<UBoxComponent>(this, TEXT("LaneArea"));
+	check(LaneArea);
+	LaneArea->OnComponentBeginOverlap.AddDynamic(this, &ALane::OnLaneOverlapBegin);
+	LaneArea->ShapeColor = FColor::Green;
+	RootComponent = LaneArea;
 
-	DespawnArea->OnComponentBeginOverlap.AddDynamic(this, &ALane::OnOverlapBegin);
+	// Objects entering the despawn area will be destroyed
+	DespawnArea = ObjectInitializer.CreateDefaultSubobject<UBoxComponent>(this, TEXT("DespawnArea"));
+	check(DespawnArea);
+	DespawnArea->SetupAttachment(LaneArea);
+	DespawnArea->ShapeColor = FColor::Red;
+	DespawnArea->OnComponentBeginOverlap.AddDynamic(this, &ALane::OnDespawnOverlapBegin);
+
+#if WITH_EDITORONLY_DATA
+	ArrowComponent = CreateEditorOnlyDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
+	if (ArrowComponent)
+	{
+		ArrowComponent->ArrowColor = FColor(150, 200, 255);
+		ArrowComponent->bTreatAsASprite = true;
+		ArrowComponent->SpriteInfo.Category = TEXT("Gameplay");
+		//ArrowComponent->SpriteInfo.DisplayName = FName("Spawner");
+		ArrowComponent->SetupAttachment(LaneArea);
+		ArrowComponent->bIsScreenSizeScaled = true;
+
+		ArrowComponent->SetRelativeTransform(SpawnTransform);
+	}
+#endif
 }
 
 // Called when the game starts or when spawned
@@ -24,8 +50,6 @@ void ALane::BeginPlay()
 
 	const bool isLooping = true;
 	GetWorldTimerManager().SetTimer(SpawnTimerHandle, this, &ALane::SpawnCollidingActor, TimeBetweenSpawns, isLooping);
-
-	
 }
 
 void ALane::EndPlay(const EEndPlayReason::Type EndPlayReason)
@@ -53,10 +77,36 @@ void ALane::SpawnCollidingActor()
 	}
 }
 
-void ALane::OnOverlapBegin(class UPrimitiveComponent* thisComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+void ALane::OnDespawnOverlapBegin(UPrimitiveComponent* thisComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	if (OtherActor != nullptr && OtherActor != this)
 	{
 		OtherActor->Destroy();
 	}
 }
+
+void ALane::OnLaneOverlapBegin(UPrimitiveComponent* thisComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor != nullptr && OtherActor != this)
+	{
+		OnFirstTimeEnteredByPlayer();
+	}
+}
+
+void ALane::OnFirstTimeEnteredByPlayer_Implementation()
+{
+
+}
+
+#if WITH_EDITORONLY_DATA
+void ALane::PostEditChangeProperty(FPropertyChangedEvent& propertyChangedEvent)
+{
+	Super::PostEditChangeProperty(propertyChangedEvent);
+
+	FName propertyName = (propertyChangedEvent.Property != NULL) ? propertyChangedEvent.Property->GetFName() : NAME_None;
+	if (propertyName == GET_MEMBER_NAME_CHECKED(ALane, SpawnTransform))
+	{
+		ArrowComponent->SetRelativeTransform(SpawnTransform);
+	}
+}
+#endif
--- Source/Frogger/Public/Gameplay/Lane.h
+++ Source/Frogger/Public/Gameplay/Lane.h
@@ -15,7 +15,10 @@ public:
 	// Sets default values for this actor's properties
 	ALane(const FObjectInitializer& ObjectInitializer);
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay, Meta = (MakeEditWidget = true))
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Gameplay)
+	class UBoxComponent* LaneArea;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Gameplay)
 	class UBoxComponent* DespawnArea;
 
 	// default object to spawn with
@@ -25,8 +28,18 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
 	float TimeBetweenSpawns;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay, Meta = (MakeEditWidget = true))
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Gameplay, Meta = (MakeEditWidget = true))
 	FTransform SpawnTransform;
+
+	UFUNCTION(BlueprintNativeEvent, Category = Gameplay)
+	void OnFirstTimeEnteredByPlayer();
+	void OnFirstTimeEnteredByPlayer_Implementation();
+
+private:
+#if WITH_EDITORONLY_DATA
+	UPROPERTY()
+	class UArrowComponent* ArrowComponent;
+#endif
 
 protected:
 	// Called when the game starts or when spawned
@@ -34,13 +47,20 @@ protected:
 
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
+#if WITH_EDITORONLY_DATA
+	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
 	FTimerHandle SpawnTimerHandle;
 
 private:
 	void SpawnCollidingActor();
 
 	UFUNCTION()
-	void OnOverlapBegin(class UPrimitiveComponent* thisComponent, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	void OnDespawnOverlapBegin(class UPrimitiveComponent* thisComponent, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnLaneOverlapBegin(class UPrimitiveComponent* thisComponent, class AActor* OtherActor, class UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 public:	
 	// Called every frame
